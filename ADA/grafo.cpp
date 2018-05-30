77bf1d27db92761e6c8108548028ac6e7e65be66
harudes <luis.rendon@ucsp.edu.pe>
2018-05-30 10:50:20 -0500

    Dijkstra y Kruskal

--- ADA/grafo.cpp
+++ ADA/grafo.cpp
@@ -3,19 +3,87 @@
 #include <fstream>
 #include <string>
 #include <ctime>
+#include <algorithm>
+#include <queue>
+#include <utility> 
 using namespace std;
 
-struct Edge  { int u, v; float w;
-Edge(int u = -1, int v = -1, float w = -1.0) : u(u), v(v), w(w) { }
+struct Edge {
+	int u, v; float w;
+	Edge(int u = -1, int v = -1, float w = -1.0) : u(u), v(v), w(w) { }
+};
+
+class compare {
+public:
+	inline bool operator()(pair<int, float> a, pair<int, float> b) {
+		return a.second>b.second;
+	}
+};
+class compare2 {
+public:
+	inline bool operator()(Edge a, Edge b) {
+		return a.w>b.w;
+	}
 };
 
 template<class T>
-int find(vector<T> vec, T x){
-	for(int i=0;i<vec.size();i++){
-		if(vec[i]==x)
+int find(vector<T> vec, T x) {
+	for (unsigned int i = 0; i<vec.size(); i++) {
+		if (vec[i] == x)
 			return i;
 	}
 	return -1;
+}
+
+template<class T>
+void insertar2(vector<T> &vec, T x) {
+	int i = 0;
+	for (; i<vec.size() && x<vec[i]; i++) {}
+	vec.insert(vec.begin() + i, x);
+}
+
+template<class T>
+bool buscar2(vector<T> &vec, T x) {
+	int l = 0, r = vec.size() - 1, i;
+	while (l<r) {
+		i = (l + r) / 2;
+		if (vec[i] == x)
+			return 1;
+		if (vec[i]>x)
+			r = i - 1;
+		else
+			l = i + 1;
+	}
+	return 0;
+}
+
+typedef vector <int> vi;
+vi pset;
+
+void init(int N)
+{
+	pset.assign(N, 0);
+	for (int i = 0; i < N; i++)
+	{
+		pset[i] = i;
+	}
+}
+
+int find_set(int i)
+{
+	if (pset[i] == i)
+	{
+		return pset[i];
+	}
+	return pset[i] = find_set(pset[i]);
+}
+bool issameset(int i, int j)
+{
+	return find_set(i) == find_set(j);
+}
+void joinset(int i, int j)
+{
+	pset[find_set(i)] = find_set(j);
 }
 
 class GRAPH {
@@ -27,162 +95,168 @@ protected:
 	int Vcnt, Ecnt;
 	bool digraph;
 public:
-	GRAPH(int V, bool digrafo):weight(V),Vcnt(V),Ecnt(0),digraph(digrafo){
-		camino.assign(V,0);
-		pesos.assign(V,100000.0);
-		disp.assign(V,"unvisited");
+	GRAPH(int V, bool digrafo) :weight(V), Vcnt(V), Ecnt(0), digraph(digrafo) {
+		camino.assign(V, -1);
+		pesos.assign(V, 100000.0);
+		disp.assign(V, "unvisited");
+		init(V);
 	}
-	void clean(){
-		for(int i=0; i<Vcnt;i++){
-			disp[i]="unvisited";
-			pesos[i]=100000.0;
-			camino[i]=0;
+	void clean() {
+		for (int i = 0; i<Vcnt; i++) {
+			disp[i] = "unvisited";
+			pesos[i] = 100000.0;
+			camino[i] = 0;
 		}
 	};
-	~GRAPH(){};
-	int V()const{
+	~GRAPH() {};
+	int V()const {
 		return Vcnt;
 	};
-	int E()const{
+	int E()const {
 		return Ecnt;
 	};
-	bool directed()const{
+	bool directed()const {
 		return digraph;
 	};
-	virtual void insert(Edge e)=0;
-	virtual void remove(Edge e)=0;
-	virtual bool edge(int u, int v)=0;
-	virtual vector<int> neightbors(int u)=0;
-	virtual void dfs(int u)=0;
-	virtual void bfs(int s)=0;
-	virtual float get_peso(int u, int v)=0;
-	int menor_peso(){
-		int x=-1,aux=0,min=0;
-		for(int i=0;i<pesos.size();i++){
-			if(disp[i]!="done"){
-				aux=i;
-				break;
+	virtual void insert(Edge e) = 0;
+	virtual void remove(Edge e) = 0;
+	virtual bool edge(int u, int v) = 0;
+	virtual vector<int> neightbors(int u) = 0;
+	virtual void dfs(int u) = 0;
+	virtual void bfs(int s) = 0;
+	virtual void llenar_cola(priority_queue<Edge, vector<Edge>, compare2> &cola) = 0;
+	virtual float get_peso(int u, int v) = 0;
+	void recorrido(int s) {
+		if (camino[s] >= 0) {
+			vector<int> nodos;
+			while (s != camino[s]) {
+				nodos.push_back(s);
+				s = camino[s];
 			}
-		}
-		min=aux;
-		for(int i=0;i<pesos.size();i++){
-			if(disp[i]!="done"){
-				if(pesos[i]<pesos[min])
-					min=i;
-			}
-		}
-		if(disp[0]!="done"||aux>0)
-			x=min;
-		return x;
-	}
-	void recorrido(int s){
-		vector<int> nodos;
-		while(s!=camino[s]){
 			nodos.push_back(s);
-			s=camino[s];
+			for (int i = nodos.size() - 1; i >= 0; i--) {
+				cout << nodos[i] << ",";
+			}
+			cout << endl;
 		}
-		nodos.push_back(s);
-		for(int i=nodos.size()-1;i>=0;i--){
-			cout<<nodos[i]<<",";
-		}
-		cout<<endl;
+		else
+			cout << "No hay un camino que llegue al nodo " << s << endl;
 	}
-	void dijkstra2(int a, int b, float peso){
+	void dijkstra(int s, int b) {
+		camino[s] = s;
+		pesos[s] = 0;
 		vector<int> vecinos;
-		int s=a;
-		disp[s]="done";
-		if(s==b){
-			cout<<"Hacia "<<s<<": "<<peso+0.0<<endl;
-			cout<<"Recorrido: "<<endl;
-			recorrido(s);
-		}
-		else{
-			vecinos=neightbors(s);
-			pesos[s]=peso;
-			for(unsigned int i=0;i<vecinos.size();i++){
-				if(disp[vecinos[i]]!="done"){
-					if(pesos[s]+get_peso(s,vecinos[i])<pesos[vecinos[i]]){
-						pesos[vecinos[i]]=pesos[s]+get_peso(s,vecinos[i]);
-						camino[vecinos[i]]=s;
+		float peso;
+		float aux;
+		priority_queue<pair<int, float>, vector<pair<int, float> >, compare > cola;
+		do {
+			disp[s] = "done";
+			peso = pesos[s];
+			vecinos = neightbors(s);
+			for (unsigned int i = 0; i<vecinos.size(); i++) {
+				if (disp[vecinos[i]] != "done") {
+					aux = pesos[s] + get_peso(s,i);
+					if (aux<pesos[vecinos[i]]) {
+						pesos[vecinos[i]] = aux;
+						camino[vecinos[i]] = s;
+						cola.push(make_pair(vecinos[i], pesos[vecinos[i]]));
 					}
 				}
 			}
-			int menor=menor_peso();
-			if(menor>=0)
-				dijkstra2(menor,b,pesos[menor]);
-		}
-	};
-	void dijkstra(int s, int b, float peso){
-		camino[s]=s;
-		dijkstra2(s,b,peso);
+			s = cola.top().first;
+			cola.pop();
+		} while (!cola.empty());
+		cout << "Hacia " << b << ": " << pesos[b] << endl;
+		cout << "Recorrido: " << endl;
+		recorrido(b);
 		//clean();
-		cout<<endl;
+		cout << endl;
 	};
+	void Kruskal() {
+		float peso_total = 0;
+		Edge temp;
+		priority_queue<Edge, vector<Edge>, compare2> cola;
+		llenar_cola(cola);
+		vector<Edge> MST;
+		while (!cola.empty()) {
+			temp = cola.top();
+			cola.pop();
+			if (!issameset(temp.u, temp.v)) {
+				MST.push_back(temp);
+				joinset(temp.u, temp.v);
+			}
+		}
+		/*for (unsigned int i = 0; i<MST.size(); i++) {
+		cout << MST[i].u << " - " << MST[i].v << endl;
+		peso_total += MST[i].w;
+		}*/
+		cout << "Peso total: " << peso_total << endl;
+	}
 };
 
-class GRAPH_matriz : public GRAPH{
+class GRAPH_matriz : public GRAPH {
 private:
 	vector<vector <bool> > adj;
 public:
-	GRAPH_matriz(int V, bool digrafo = false):adj(V),GRAPH(V,digrafo){
-		for (int i = 0; i < V; i++){
+	GRAPH_matriz(int V, bool digrafo = false) :adj(V), GRAPH(V, digrafo) {
+		for (int i = 0; i < V; i++) {
 			adj[i].assign(V, false);
 			weight[i].assign(V, 0);
 		}
 	}
-	~GRAPH_matriz(){};
-	void insert(Edge e){
-		adj[e.u][e.v]=1;
-		weight[e.u][e.v]=e.w;
-		if(digraph){
-			adj[e.v][e.u]=1;
-			weight[e.v][e.u]=e.w;
+	~GRAPH_matriz() {};
+	void insert(Edge e) {
+		adj[e.u][e.v] = 1;
+		weight[e.u][e.v] = e.w;
+		if (digraph) {
+			adj[e.v][e.u] = 1;
+			weight[e.v][e.u] = e.w;
 		}
 		Ecnt++;
 	};
-	void remove(Edge e){
-		adj[e.v][e.w]=0;
-		if(digraph)
-			adj[e.w][e.v]=0;
+	void remove(Edge e) {
+		adj[e.u][e.v] = 0.0;
+		if (digraph)
+			adj[e.v][e.u] = 0.0;
 		Ecnt--;
 	};
-	bool edge(int u, int v){
+	bool edge(int u, int v) {
 		return adj[u][v];
 	};
-	vector<int> neightbors(int u){
+	vector<int> neightbors(int u) {
 		vector<int> vecinos;
-		for(int i=0;i<Vcnt;i++){
-			if(adj[u][i])
+		for (int i = 0; i<Vcnt; i++) {
+			if (adj[u][i])
 				vecinos.push_back(i);
 		}
 		return vecinos;
 	}
-	void dfs(int u){
-		cout<<u<<" -> ";
+	void dfs(int u) {
+		cout << u << " -> ";
 		disp[u] = "in_progress";
-		vector<int> vecinos=neightbors(u);
-		for(int i=0;i<vecinos.size();i++){
-			if(disp[vecinos[i]]=="unvisited"){
+		vector<int> vecinos = neightbors(u);
+		for (unsigned int i = 0; i<vecinos.size(); i++) {
+			if (disp[vecinos[i]] == "unvisited") {
 				dfs(vecinos[i]);
 			}
 		}
-		disp[u]="done";
+		disp[u] = "done";
 	};
-	void bfs(int s){
+	void bfs(int s) {
 		vector<vector<int> > L(Vcnt);
 		L[0].push_back(s);
-		for(int i=0;i<Vcnt;i++){
-			for(int j=0; j<L[i].size();i++){
-				vector<int> vecinos=neightbors(L[i][j]);
-				for(int k=0;k<vecinos.size();k++){
-					disp[vecinos[k]]="visited";
-					L[i+1].push_back(vecinos[k]);
-					cout<<vecinos[k]<<" -> ";
+		for (int i = 0; i<Vcnt; i++) {
+			for (unsigned int j = 0; j<L[i].size(); i++) {
+				vector<int> vecinos = neightbors(L[i][j]);
+				for (unsigned int k = 0; k<vecinos.size(); k++) {
+					disp[vecinos[k]] = "visited";
+					L[i + 1].push_back(vecinos[k]);
+					cout << vecinos[k] << " -> ";
 				}
 			}
 		}
 	};
-	float get_peso(int u, int v){
+	float get_peso(int u, int v) {
 		return weight[u][v];
 	};
 	class adjIterator {
@@ -190,7 +264,7 @@ public:
 		const GRAPH_matriz &G;
 		int i, v;
 	public:
-		adjIterator(const GRAPH_matriz &G, int v) :  G(G), v(v), i(-1) { }
+		adjIterator(const GRAPH_matriz &G, int v) : G(G), v(v), i(-1) { }
 		int beg() { i = -1; return nxt(); }
 		int nxt() {
 			for (i++; i < G.V(); i++)
@@ -201,74 +275,82 @@ public:
 	};
 };
 
-class GRAPH_lista : public GRAPH{
+class GRAPH_lista : public GRAPH {
 private:
 	vector<vector<int> > adj;
 public:
-	GRAPH_lista(int V, bool digrafo = false):adj(V),GRAPH(V,digrafo) {};
-	~GRAPH_lista(){};
-	void insert(Edge e){
+	GRAPH_lista(int V, bool digrafo = false) :adj(V), GRAPH(V, digrafo) {};
+	~GRAPH_lista() {};
+	void insert(Edge e) {
 		adj[e.u].push_back(e.v);
 		weight[e.u].push_back(e.w);
-		if(digraph){
+		if (digraph) {
 			adj[e.v].push_back(e.u);
 			weight[e.v].push_back(e.w);
 		}
 		Ecnt++;
 	};
-	void remove(Edge e){
-		adj[e.v][e.w]=0;
-		if(digraph)
-			adj[e.w][e.v]=0;
-		Ecnt--;
+	void remove(Edge e) {
+		/*adj[e.v][e.w] = 0;
+		if (digraph)
+		adj[e.w][e.v] = 0;
+		Ecnt--;*/
 	};
-	bool edge(int u, int v){
-		return find(adj[u],v)>=0;
+	bool edge(int u, int v) {
+		return find(adj[u], v) >= 0;
 	};
-	vector<int> neightbors(int u){
+	vector<int> neightbors(int u) {
 		return adj[u];
 	}
-	void dfs(int u){
-		cout<<u<<" -> ";
+	void dfs(int u) {
+		cout << u << " -> ";
 		disp[u] = "in_progress";
-		vector<int> vecinos=neightbors(u);
-		for(int i=0;i<vecinos.size();i++){
-			if(disp[vecinos[i]]=="unvisited"){
+		vector<int> vecinos = neightbors(u);
+		for (unsigned int i = 0; i<vecinos.size(); i++) {
+			if (disp[vecinos[i]] == "unvisited") {
 				dfs(vecinos[i]);
 			}
 		}
-		disp[u]="done";
+		disp[u] = "done";
 	};
-	void bfs(int s){
+	void bfs(int s) {
 		vector<vector<int> > L(Vcnt);
 		L[0].push_back(s);
-		for(int i=0;i<Vcnt;i++){
-			for(int j=0; j<L[i].size();i++){
-				vector<int> vecinos=neightbors(L[i][j]);
-				for(int k=0;k<vecinos.size();k++){
-					disp[vecinos[k]]="visited";
-					L[i+1].push_back(vecinos[k]);
-					cout<<vecinos[k]<<" -> ";
+		for (int i = 0; i<Vcnt; i++) {
+			for (unsigned int j = 0; j<L[i].size(); i++) {
+				vector<int> vecinos = neightbors(L[i][j]);
+				for (unsigned int k = 0; k<vecinos.size(); k++) {
+					disp[vecinos[k]] = "visited";
+					L[i + 1].push_back(vecinos[k]);
+					cout << vecinos[k] << " -> ";
 				}
 			}
 		}
 	};
-	float get_peso(int u, int v){
-		return weight[u][find(adj[u],v)];
+	inline float get_peso(int u, int v) {
+		return weight[u][v];
+	}
+	void llenar_cola(priority_queue<Edge, vector<Edge>, compare2> &cola) {
+		Edge arista;
+		for (int i = 0; i<Vcnt; i++) {
+			for (unsigned int j = 0; j<adj[i].size(); j++) {
+				cola.push(Edge(i, adj[i][j], weight[i][j]));
+			}
+		}
 	}
 	/*class adjIterator {
 	private:
-		const GRAPH_matriz &G;
-		int i, v;
+	const GRAPH_matriz &G;
+	int i, v;
 	public:
-		adjIterator(const GRAPH_matriz &G, int v) :  G(G), v(v), i(-1) { }
-		int beg() { i = -1; return nxt(); }
-		int nxt() {
-			for (i++; i < G.V(); i++)
-				if (G.adj[v][i] == true) return i;
-			return -1;
-		}
-		bool end() { return i >= G.V(); }
+	adjIterator(const GRAPH_matriz &G, int v) :  G(G), v(v), i(-1) { }
+	int beg() { i = -1; return nxt(); }
+	int nxt() {
+	for (i++; i < G.V(); i++)
+	if (G.adj[v][i] == true) return i;
+	return -1;
+	}
+	bool end() { return i >= G.V(); }
 	};	*/
 };
 
@@ -292,8 +374,8 @@ void show(const Graph &G) {
 	for (int s = 0; s < G.V(); s++) {
 		cout.width(2); cout << s << ":";
 		typename Graph::adjIterator A(G, s);
-		for (int t = A.beg(); !A.end(); t = A.nxt()) { 
-			cout.width(2); cout << t << " "; 
+		for (int t = A.beg(); !A.end(); t = A.nxt()) {
+			cout.width(2); cout << t << " ";
 		}
 		cout << endl;
 	}
@@ -301,29 +383,29 @@ void show(const Graph &G) {
 
 
 template <class G>
-void insertar(G grafo, string nodo){
+void insertar(G grafo, string nodo) {
 	
 }
 
-int main(int argc, char *argv[]){
-	unsigned t0,t1;
-	int v,e;
-	cin>>v>>e;
-	int a,b;
+int main(int argc, char *argv[]) {
+	unsigned t0, t1;
+	int v, e;
+	cin >> v >> e;
+	int a, b;
 	float w;
-	GRAPH_lista grafo(v,0);
-	for(int i=0;i<e;i++){
-		cin>>a>>b>>w;
-		Edge ed(a,b,w+0.0);
+	GRAPH_lista grafo(v, 1);
+	for (int i = 0; i<e; i++) {
+		cin >> a >> b >> w;
+		Edge ed(a, b, w);
 		grafo.insert(ed);
 	}
 	//show(grafo);
-	cout<<"Grafo creado"<<endl;
-	t0=clock();
-	grafo.dijkstra(10,100,0.0);
-	t1=clock();
-	double time = (double(t1-t0)/CLOCKS_PER_SEC);
-	cout << "Tiempo: " << time<<endl;
+	cout << "Grafo creado" << endl;
+	t0 = clock();
+	grafo.dijkstra(10,100);
+	t1 = clock();
+	double time = (double(t1 - t0) / CLOCKS_PER_SEC);
+	cout << "Tiempo: " << time << endl;
 	return 0;
 }
 
